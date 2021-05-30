25895f1baf366303746916cda251c534b7a0b1ad
Mak <makarand.kate@gmail.com>
2021-05-30 19:18:10 +0530

    init

---
+++ android/app/src/main/java/in/co/pecule/booking/MainActivity.java
@@ -0,0 +1,5 @@
+package in.co.pecule.booking;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.co.pecule.booking',
+  appName: 'cowin-booking',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
